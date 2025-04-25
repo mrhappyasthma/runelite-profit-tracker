3e53af6dc02c55d9eec2946713d4563dbad23304
Neil Harbin <neilharbin0@gmail.com>
2025-04-25 08:23:30 -0400

    -Feature- Added configuration setting for shortening drop values -Fix- Fixed negative symbol being partially hidden by coin sprite for values formatted to 5 characters long like -1.2K or -100K -Feature- Made drop icon adjust dynamically with coin value, along with a configuration option

--- src/main/java/com/profittracker/ProfitTrackerConfig.java
+++ src/main/java/com/profittracker/ProfitTrackerConfig.java
@@ -30,5 +30,25 @@ public interface ProfitTrackerConfig extends Config
     {
         return true;
     }
+
+    @ConfigItem(
+            keyName = "shortDrops",
+            name = "Shorten drop numbers",
+            description = "Shorten drop numbers like 1.2K instead of 1,223, or 10M instead of 10,000,000"
+    )
+    default boolean shortDrops()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "iconStyle",
+            name = "Icon style",
+            description = "Dynamically adjust the coin icon based on the drop value, or select a specific icon."
+    )
+    default ProfitTrackerIconType iconStyle()
+    {
+        return ProfitTrackerIconType.DYNAMIC;
+    }
 }
 
---
+++ src/main/java/com/profittracker/ProfitTrackerIconType.java
@@ -0,0 +1,15 @@
+package com.profittracker;
+
+public enum ProfitTrackerIconType {
+    DYNAMIC,
+    COINS_1,
+    COINS_2,
+    COINS_3,
+    COINS_4,
+    COINS_5,
+    COINS_25,
+    COINS_100,
+    COINS_250,
+    COINS_1000,
+    COINS_10000
+}
