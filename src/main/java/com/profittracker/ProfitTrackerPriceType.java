42e57fbbb77d00997cc1740f92c9244924627688
Neil Harbin <neilharbin0@gmail.com>
2025-08-02 21:11:30 -0400

    -Change- Modified text for various shop values to more clearly indicate rates

--- src/main/java/com/profittracker/ProfitTrackerPriceType.java
+++ src/main/java/com/profittracker/ProfitTrackerPriceType.java
@@ -3,10 +3,10 @@ package com.profittracker;
 public enum ProfitTrackerPriceType {
     GE("Grand Exchange"),
     GE_TAXED("Grand Exchange (2% Tax)"), //98% rounded up
-    HIGH_ALCH("High Alchemy / Shop++"), //60%, wildy shop, rogues den
-    SHOP_SPECIAL("Shop+"), //55%, west ardy, pollnivneach, legends' guild general stores
-    LOW_ALCH("Low Alchemy / Shop"), //40%, regular general stores
-    SHOP_OVERSTOCK("Shop (Overstocked)"); //10%
+    HIGH_ALCH("High Alchemy / Shop 60%"), //60%, wildy shop, rogues den
+    SHOP_SPECIAL("Shop 55%"), //55%, west ardy, pollnivneach, legends' guild general stores, moon clan, lighthouse
+    LOW_ALCH("Low Alchemy / Shop 40%"), //40%, regular general stores
+    SHOP_OVERSTOCK("Shop 10% (Overstocked)"); //10%
 
     private final String name;
 
