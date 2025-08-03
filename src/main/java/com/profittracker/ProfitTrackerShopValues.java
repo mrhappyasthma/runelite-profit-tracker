a93b567fed02c0f90b9faa846ffd94c562c34249
Neil Harbin <neilharbin0@gmail.com>
2025-08-02 21:12:45 -0400

    -Refactor- Cleanup and magic number consolidation

---
+++ src/main/java/com/profittracker/ProfitTrackerShopValues.java
@@ -0,0 +1,14 @@
+package com.profittracker;
+
+//See https://oldschool.runescape.wiki/w/General_store
+public class ProfitTrackerShopValues {
+    public static final double RASOLO = 0.05;
+    public static final double MINIMUM_PRICE = 0.1; //Lowest the price can supposedly drop when overstocking a shop
+    public static final double CANIFIS = 0.1;
+    public static final double STAN = 0.15;
+    public static final double DWARVEN = 0.3;
+    public static final double COMMON_LOW_ALCH = 0.4; //Most general stores
+    public static final double SPECIAL_50 = 0.5; //At least 4 different shops
+    public static final double SPECIAL_55 = 0.55; //At least 8 different shops
+    public static final double SPECIAL_60_HIGH_ALCH = 0.6; //At least 4 different shops
+}
